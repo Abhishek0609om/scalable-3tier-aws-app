aea696af617d1a9c1fc567701f8b03c54992569b
EC2 Default User <ec2-user@ip-10-0-7-2.ec2.internal>
2026-04-10 04:43:48 +0000

    fix: correct docker image tag

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const server = http.createServer((req, res) => {
   res.end("CI/CD is working");
 });
 
-server.listen(3000, () => {
+server.listen(3000, "0.0.0.0", () => {
   console.log("Server running on port 3000");
 });
